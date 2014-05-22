d4a9914f00377f2975f6e15917e08a2415ae61a0
Rakesh.P.D <errakeshpd@gmail.com>
2014-05-22 11:13:04 +0530

    expertise page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/aboutus' => 'home#about'
+  get '/Expertise' => 'home#expertise'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
