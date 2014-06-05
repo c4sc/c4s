420767f4d89d7598c7f04c9433e9747e22bc2c3e
Rakesh.P.D <errakeshpd@gmail.com>
2014-06-05 18:15:58 +0530

    admin home page data retreived and contact list updated

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -3,6 +3,8 @@ class AdministratorController < ApplicationController
     before_filter :authenticate_user!
 
     def index
+        @data = Contact.all
+
         render :layout => "layouts/administrator"
     end
 end
