a3b61933325364ad835bfd22ed2de48cc4ba2037
Rakesh.P.D <errakeshpd@gmail.com>
2014-06-05 16:44:22 +0530

    user authorization to the admin block

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -1,8 +1,8 @@
 class AdministratorController < ApplicationController
 
-before_filter :authenticate_user!
+    before_filter :authenticate_user!
 
-def index
-    render :layout => "layouts/administrator"
-end
+    def index
+        render :layout => "layouts/administrator"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,73 +1,74 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :path => '', :path_names => {:sign_in => '/administrator/login', :sign_out => '/administrator/logout_session'}
-  resources :expertises
-  resources :contacts
+    devise_for :users, :path => '', :path_names => {:sign_in => '/administrator/login', :sign_out => '/administrator/logout_session'}
+    resources :expertises
+    resources :contacts
 
-  root 'home#index'
-  get '/aboutus' => 'home#about'
-  get '/Expertise' => 'home#expertise'
-  get '/works' => 'home#work'
-  get '/works/arividam' => 'home#arividam'
-  get '/works/lms' => 'home#lms'
-  get '/works/peecs' => 'home#peecs'
-  get '/c4s/contact' => 'home#contact'
-  get '/c4s/services' => 'home#services'
-  
-  get '/admin/expertise/5624_view' => 'administrator#index'
-  get '/admin/contacts/245226_index' => 'administrator#admin_expertise_list'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+    root 'home#index'
+    get '/aboutus' => 'home#about'
+    get '/Expertise' => 'home#expertise'
+    get '/works' => 'home#work'
+    get '/works/arividam' => 'home#arividam'
+    get '/works/lms' => 'home#lms'
+    get '/works/peecs' => 'home#peecs'
+    get '/c4s/contact' => 'home#contact'
+    get '/c4s/services' => 'home#services'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    get '/admin/expertise/5624_view' => 'administrator#index'
+    get '/admin/contacts/245226_index' => 'administrator#admin_expertise_list'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # The priority is based upon order of creation: first created -> highest priority.
+    # See how all your routes lay out with "rake routes".
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+    # You can have the root of your site routed with "root"
+    # root 'welcome#index'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    # Example of regular route:
+    #   get 'products/:id' => 'catalog#view'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    # Example of named route that can be invoked with purchase_url(id: product.id)
+    #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+    # Example resource route (maps HTTP verbs to controller actions automatically):
+    #   resources :products
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+    # Example resource route with options:
+    #   resources :products do
+    #     member do
+    #       get 'short'
+    #       post 'toggle'
+    #     end
+    #
+    #     collection do
+    #       get 'sold'
+    #     end
+    #   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+    # Example resource route with sub-resources:
+    #   resources :products do
+    #     resources :comments, :sales
+    #     resource :seller
+    #   end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    # Example resource route with more complex sub-resources:
+    #   resources :products do
+    #     resources :comments
+    #     resources :sales do
+    #       get 'recent', on: :collection
+    #     end
+    #   end
+
+    # Example resource route with concerns:
+    #   concern :toggleable do
+    #     post 'toggle'
+    #   end
+    #   resources :posts, concerns: :toggleable
+    #   resources :photos, concerns: :toggleable
+
+    # Example resource route within a namespace:
+    #   namespace :admin do
+    #     # Directs /admin/products/* to Admin::ProductsController
+    #     # (app/controllers/admin/products_controller.rb)
+    #     resources :products
+    #   end
 end
