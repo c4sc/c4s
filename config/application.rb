ca4afec539ac8f1fdd068accbc1601eb5f9cb9de
Rakesh.P.D <errakeshpd@gmail.com>
2014-06-06 07:18:18 +0530

    assets compiled setup included

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module C4s
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-config.assets.initialize_on_precompile = false
+config.assets.enabled = true
+config.assets.precompile += %w( *.css *.js )
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
