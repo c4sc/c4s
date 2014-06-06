6470742cc698c632966b6c316b08b5409b662c5e
Rakesh.P.D <errakeshpd@gmail.com>
2014-06-06 08:55:38 +0530

    precompile desabled

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module C4s
     # config.time_zone = 'Central Time (US & Canada)'
 config.assets.enabled = true
 config.assets.precompile += %w( *.css *.js )
+config.assets.initialize_on_precompile = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
